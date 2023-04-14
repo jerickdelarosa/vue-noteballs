fd4e472d0099dc063d43265108fb15e0016d6757
Jerick Dela Rosa <delarosajerick26@gmail.com>
2023-04-14 16:49:40 +0800

    Data Methods, Child Components, Computed & Emit

--- src/views/ViewNotes.vue
+++ src/views/ViewNotes.vue
@@ -1,36 +1,74 @@
 <template>
-    <div class="notes">
-        <div class="card has-background-success-dark p-4 mb-5">
-            <div class="field">
-                <div class="control">
-                    <textarea class="textarea" placeholder="Add a new note" />
-                </div>
-            </div>
-            <div class="field is-grouped is-grouped-right">
-                <div class="control">
-                    <button class="button is-link has-background-success">
-                        Add New Note
-                    </button>
-                </div>
-            </div>
+  <div class="notes">
+    <div class="card has-background-success-dark p-4 mb-5">
+      <div class="field">
+        <div class="control">
+          <textarea
+            v-model="newNote"
+            class="textarea"
+            placeholder="Add a new note"
+            ref="newNoteRef"
+          />
         </div>
-
-        <div v-for="i in 3" class="card mb-4">
-            <div class="card-content">
-                <div class="content">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium doloremque, sunt earum quidem et
-                    vero quae maxime distinctio delectus debitis, quisquam quaerat modi nostrum impedit facere quia
-                    repudiandae molestiae illo?
-                </div>
-            </div>
-            <footer class="card-footer">
-                <a href="#" class="card-footer-item">Edit</a>
-                <a href="#" class="card-footer-item">Delete</a>
-            </footer>
+      </div>
+      <div class="field is-grouped is-grouped-right">
+        <div class="control">
+          <button
+            @click="addNote"
+            :disabled="!newNote"
+            class="button is-link has-background-success"
+          >
+            Add New Note
+          </button>
         </div>
+      </div>
     </div>
+
+    <Note
+      v-for="note in notes"
+      :key="`note-key-${note.id}`"
+      :note="note"
+      @deleteClicked="deleteNote"
+    />
+  </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
+import Note from "@/components/notes/Note.vue";
 
+/* Notes */
+const newNoteRef = ref(null);
+const newNote = ref("");
+const notes = ref([
+  {
+    id: "id1",
+    content:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantiu doloremque, sunt earum quidem et vero quae maxime distinctio delectu debitis, quisquam quaerat modi nostrum impedit facere quia repudianda molestiae illo?",
+  },
+  {
+    id: "id2",
+    content: "This is a shorter notes!",
+  },
+]);
+
+const addNote = () => {
+  let currentDate = new Date().getTime(),
+    id = currentDate.toString();
+
+  let note = {
+    id: id,
+    content: newNote.value,
+  };
+
+  notes.value.unshift(note);
+  newNote.value = "";
+  newNoteRef.value.focus();
+};
+
+const deleteNote = (idToDelete) => {
+  notes.value = notes.value.filter((note) => {
+    return note.id !== idToDelete;
+  });
+};
 </script>
