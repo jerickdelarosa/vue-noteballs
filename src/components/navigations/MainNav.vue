8f3afe20d0f8c79a80941c038974e8f732f91cab
Jerick Dela Rosa <delarosajerick26@gmail.com>
2023-04-18 13:11:26 +0800

    VueUse onClickOutside; end of composables.

--- src/components/navigations/MainNav.vue
+++ src/components/navigations/MainNav.vue
@@ -12,6 +12,7 @@
           aria-label="menu"
           aria-expanded="false"
           data-target="navbarBasicExample"
+          ref="navbarBurgerRef"
         >
           <span aria-hidden="true"></span>
           <span aria-hidden="true"></span>
@@ -23,6 +24,7 @@
         id="navbarBasicExample"
         class="navbar-menu"
         :class="{ 'is-active': showMobileNav }"
+        ref="navbarMenuRef"
       >
         <div class="navbar-end">
           <RouterLink
@@ -48,8 +50,25 @@
 
 <script setup>
 import { ref } from "vue";
+import { onClickOutside } from "@vueuse/core";
 
 const showMobileNav = ref(false);
+const navbarMenuRef = ref(null);
+const navbarBurgerRef = ref(null);
+
+onClickOutside(navbarMenuRef, () => {
+  showMobileNav.value = false;
+});
+
+onClickOutside(
+  navbarBurgerRef,
+  () => {
+    showMobileNav.value = false;
+  },
+  {
+    ignore: [navbarBurgerRef],
+  }
+);
 </script>
 
 <style>
